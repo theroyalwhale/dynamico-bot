6e6270514df5dd2407009337bc031ed6d229c52b
The Royal Whale <walesmemer@gmail.com>
2023-02-18 13:52:13 +0000

    ITS A BREAKTHROUGH https://platform.openai.com/docs/guides/images/usage

--- dyn_openai.py
+++ dyn_openai.py
@@ -1,6 +1,6 @@
 
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 
 def imageGen(prompt,n):
   response = openai.Image.create(
@@ -11,3 +11,11 @@ def imageGen(prompt,n):
   image_url = response['data'][0]['url']
   return image_url
 
+def imageVar(image,n):
+  response = openai.Image.create_variation(
+    image=open(image, "rb"),
+    n=n,
+    size="1024x1024"
+  )
+  image_url = response['data'][0]['url']
+  return image_url
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
 
 from dyn_openai import *
 import webbrowser
-
-while True:
-  print(imageGen(input("PROMPT PLEASE: "),1))
