65acb31af5a2d6f079e19473445109fee4b90de1
The Royal Whale <walesmemer@gmail.com>
2023-02-18 20:53:23 +0800

    Update openai.py

--- openai.py
+++ openai.py
@@ -1,3 +1,12 @@
 
 import openai
 openai.api_key = "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"
+
+def imageGen(prompt,n):
+  response = openai.Image.create(
+    prompt=prompt,
+    n=n,
+    size="1024x1024"
+  )
+  image_url = response['data'][0]['url']
+  return image_url
